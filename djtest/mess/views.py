e0956ccb7375e09688441ec85bcc7f0ec86335e8
Vasja Volin <vasja.volin@gmail.com>
2016-08-27 08:05:16 -0600

    fix api not returning 200 OK on deletion

--- djtest/mess/views.py
+++ djtest/mess/views.py
@@ -1,7 +1,12 @@
 from .models import Message
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 from .serializers import MessageSerializer
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        return Response(data='{message:"success"}', status=status.HTTP_200_OK)
