39b06391d47528b78f638de5175b09b170c071a6
Vasja Volin <vasja.volin@gmail.com>
2016-08-27 05:49:36 -0600

    fix really frustrating duplicate name issue by renaming app

--- djtest/mess/apps.py
+++ djtest/mess/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class MessagesConfig(AppConfig):
-    name = 'messages'
+class MessConfig(AppConfig):
+    name = 'mess'
